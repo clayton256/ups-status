4cfd94ce48597b261955f3e9d3d52a0ee2c89a2f
Mark Clayton <1789080+clayton256@users.noreply.github.com>
2025-11-15 20:27:09 -0500

    Initial Commit

---
+++ UPSextTests/UPSextTests.swift
@@ -0,0 +1,16 @@
+//
+//  UPSextTests.swift
+//  UPSextTests
+//
+//  Created by Mark Clayton on 11/15/25.
+//
+
+import Testing
+
+struct UPSextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
